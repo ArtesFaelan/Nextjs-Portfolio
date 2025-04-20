c4c80d7efe1b766bfeb6039647720f1d2ad3a2c8
flyingavius <arvidxian1029@gmail.com>
2025-04-20 06:17:35 -0400

    update config for gh page deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/ArtesFaelan/' : '',
+  basePath: isProd ? '/ArtesFaelan' : '',
+  output: 'export'
 };
 
 module.exports = nextConfig;
